b55fe2690429efe5858bc1b9d0b28092df8d0912
Voku199 <155540714+Voku199@users.noreply.github.com>
2025-04-17 13:46:30 +0200

    Summary

--- anglictina/app/main.py
+++ anglictina/app/main.py
@@ -19,5 +19,6 @@ app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(verbs_bp)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)
