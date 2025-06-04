a6b84af8534d2cc64b67f156585334da36004577
Voku199 <155540714+Voku199@users.noreply.github.com>
2025-06-04 17:59:32 +0200

    Update main.py

--- anglictina/app/main.py
+++ anglictina/app/main.py
@@ -93,4 +93,4 @@ def add_security_headers(response):
 # serve(app, host="0.0.0.0", port=8080, threads=32) PRO SERVER
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080, threads=32)
+    serve(app, host="0.0.0.0", port=8080, threads=24)
